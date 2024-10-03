4387f80a94d15269a5129080362447c4495e039a
Amaro Fibla <163011598+thxGalda@users.noreply.github.com>
2024-10-03 15:30:19 -0300

    Update excepcionInicializacionFallida.java

--- src/paqueteMain/excepcionInicializacionFallida.java
+++ src/paqueteMain/excepcionInicializacionFallida.java
@@ -1,6 +1,6 @@
 package paqueteMain;
 public class excepcionInicializacionFallida extends Exception {
-    excepcionInicializacionFallida(){
-        super("La inicializacion no ha podido reslizarse, intente nuevamente");
+    excepcionInicializacionFallida(msj){
+        super(msj);
     }
 }
