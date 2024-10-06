ac61f695cd16dff0b05e95e8b6d62ec1937b1d1e
Andres Garcia Muñoz <163463179+littlenegrito@users.noreply.github.com>
2024-10-06 20:46:39 -0300

    Delete src/controladores/ControladorVDePrueba.java

--- src/controladores/ControladorVDePrueba.java
+++
@@ -1,9 +0,0 @@
-package controladores;
-import modelo.Modelo;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class ControladorVDePrueba {
-	private Ventana vista;
-
-}
