969b62cfb80365f84f50af100019cfc3ea67f1c9
Andres Garcia Muñoz <163463179+littlenegrito@users.noreply.github.com>
2024-10-06 23:49:41 -0300

    Update LectorCSV.java

--- src/modelo/LectorCSV.java
+++ src/modelo/LectorCSV.java
@@ -1,4 +1,4 @@
-package paqueteMain;
+package modelo;
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -8,13 +8,72 @@ import java.util.ArrayList;
 import java.net.URL;
 
 public class LectorCSV {
-    public static List<Curso> cargarCursosDesdeCSV(String archivoCSV, List<Profesor> profesores) {
+	private List<Profesor> profesores;
+    private List<Curso> cursos;
+
+    public LectorCSV() {
+        profesores = new ArrayList<>();
+        cursos = new ArrayList<>();
+    }
+    
+    public void leerArchivoCSV(String rutaArchivo) {
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                // Procesa la línea leída
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.err.println("Error leyendo archivo: " + e.getMessage());
+        }
+    }
+    
+    public void cargarArchivosCSV(String[] archivosCSV) {
+        // Crear listas para almacenar los objetos leídos
+        List<Carpeta> carpetas = new ArrayList<>();
+        List<Estudiante> estudiantes = new ArrayList<>();
+        List<Recurso> recursos = new ArrayList<>();
+    
+        // Leer archivos CSV y agregar objetos a las listas
+        for (String archivoCSV : archivosCSV) {
+            if (archivoCSV.contains("Profesores")) {
+                profesores.addAll(cargarProfesoresDesdeCSV(archivoCSV));
+                if(profesores.isEmpty()) System.out.println("No se cargaron los profesores");
+            } else if (archivoCSV.contains("Carpetas")) {
+                carpetas.addAll(cargarCarpetasDesdeCSV(archivoCSV));
+            } else if (archivoCSV.contains("Cursos")) {
+                cursos.addAll(cargarCursosDesdeCSV(archivoCSV, profesores));
+            } else if (archivoCSV.contains("Estudiantes")) {
+                estudiantes.addAll(cargarEstudiantesDesdeCSV(archivoCSV, cursos));
+            } else if (archivoCSV.contains("Recursos")) {
+                recursos.addAll(cargarRecursosDesdeCSV(archivoCSV));
+            }
+        }
+    
+        // Agregar objetos a la lista de cursos
+        for (Curso curso : cursos) {
+        	System.out.println("Se está procesando el curso: " + curso.getNombre());
+            curso.setCarpetas(carpetas);
+            // recorrer profesores y elegir que le corresponde
+            
+            // crear lista solo los estudiante que coinciden con curso
+            curso.setEstudiantes(estudiantes);
+        }
+    
+        // Agregar objetos a la lista de carpetas
+        for (Carpeta carpeta : carpetas) {
+            carpeta.agregarRecurso(recursos);
+        }
+
+    }
+    
+    public List<Curso> cargarCursosDesdeCSV(String archivoCSV, List<Profesor> profesores) {
         List<Curso> cursos = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
             String linea;
             br.readLine(); // Saltar la primera línea (encabezados)
             while ((linea = br.readLine()) != null) {
-                String[] campos = linea.split(";");
+                String[] campos = linea.split(";"); // Dividir la línea en campos
                 Profesor profesor = null;
                 for (Profesor p : profesores) {
                     if (p.getNombre().equals(campos[2])) {
@@ -30,13 +89,13 @@ public class LectorCSV {
         }
         return cursos;
     }
-    public static List<Profesor> cargarProfesoresDesdeCSV(String archivoCSV) {
+    public List<Profesor> cargarProfesoresDesdeCSV(String archivoCSV) {
         List<Profesor> profesores = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
             String linea;
             br.readLine(); // Saltar la primera línea (encabezados)
             while ((linea = br.readLine()) != null) {
-                String[] campos = linea.split(";");
+                String[] campos = linea.split(";"); // Dividir la línea en campos
                 Profesor profesor = new Profesor(campos[0], Integer.parseInt(campos[1]), campos[2], campos[3], campos[4], Boolean.parseBoolean(campos[5]), campos[6]);
                 profesores.add(profesor);
             }
@@ -45,7 +104,7 @@ public class LectorCSV {
         }
         return profesores;
     }
-    public static List<Estudiante> cargarEstudiantesDesdeCSV(String archivoCSV, List<Curso> cursos) {
+    public List<Estudiante> cargarEstudiantesDesdeCSV(String archivoCSV, List<Curso> cursos) {
         List<Estudiante> estudiantes = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
             String linea;
@@ -62,7 +121,7 @@ public class LectorCSV {
                         }
                     }
                 }
-                Estudiante estudiante = new Estudiante(campos[0], Integer.parseInt(campos[1]), campos[2], campos[3], campos[4], Boolean.parseBoolean(campos[5]), cursosEstudiante, Integer.parseInt(campos[7]));
+                Estudiante estudiante = new Estudiante(campos[0], Integer.parseInt(campos[1]), campos[2], campos[3], campos[4], Boolean.parseBoolean(campos[5]), Integer.parseInt(campos[7]));
                 estudiantes.add(estudiante);
             }
         } catch (IOException e) {
@@ -70,7 +129,7 @@ public class LectorCSV {
         }
         return estudiantes;
     }
-    public static List<Recurso> cargarRecursosDesdeCSV(String archivoCSV) {
+    public List<Recurso> cargarRecursosDesdeCSV(String archivoCSV) {
         List<Recurso> recursos = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
             String linea;
@@ -85,7 +144,7 @@ public class LectorCSV {
         }
         return recursos;
     }
-    public static List<Carpeta> cargarCarpetasDesdeCSV(String archivoCSV) {
+    public List<Carpeta> cargarCarpetasDesdeCSV(String archivoCSV) {
         List<Carpeta> carpetas = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
             String linea;
@@ -101,77 +160,13 @@ public class LectorCSV {
         return carpetas;
     }
 
-    public static void cargarArchivosCSV(String[] archivosCSV) {
-        // Crear listas para almacenar los objetos leídos
-        List<Profesor> profesores = new ArrayList<>();
-        List<Carpeta> carpetas = new ArrayList<>();
-        List<Curso> cursos = new ArrayList<>();
-        List<Estudiante> estudiantes = new ArrayList<>();
-        List<Recurso> recursos = new ArrayList<>();
     
-        // Leer archivos CSV y agregar objetos a las listas
-        for (String archivoCSV : archivosCSV) {
-            if (archivoCSV.contains("Profesores")) {
-                profesores.addAll(cargarProfesoresDesdeCSV(archivoCSV));
-            } else if (archivoCSV.contains("Carpetas")) {
-                carpetas.addAll(cargarCarpetasDesdeCSV(archivoCSV));
-            } else if (archivoCSV.contains("Cursos")) {
-                cursos.addAll(cargarCursosDesdeCSV(archivoCSV, profesores));
-            } else if (archivoCSV.contains("Estudiantes")) {
-                estudiantes.addAll(cargarEstudiantesDesdeCSV(archivoCSV, cursos));
-            } else if (archivoCSV.contains("Recursos")) {
-                recursos.addAll(cargarRecursosDesdeCSV(archivoCSV));
-            }
-        }
-    
-        // Agregar objetos a la lista de cursos
-        for (Curso curso : cursos) {
-            curso.setCarpetas(carpetas);
-            curso.setEstudiantes(estudiantes);
-        }
-    
-        // Agregar objetos a la lista de profesores
-        for (Profesor profesor : profesores) {
-            profesor.setCursosImpartidos(cursos);
-        }
-    
-        // Agregar objetos a la lista de carpetas
-        for (Carpeta carpeta : carpetas) {
-            carpeta.agregarRecurso(recursos);
-        }
-    
-        // Agregar objetos a la lista de estudiantes
-        for (Estudiante estudiante : estudiantes) {
-            estudiante.setCursos(cursos);
-        }
-    
-        // Agregar objetos a la lista de recursos
-        for (Recurso recurso : recursos) {
-            recurso.setCurso(cursos);
-        }
-    }
-    public static void leerArchivoCSV(String url) throws IOException {
-        @SuppressWarnings("deprecation")
-        URL urlArchivo = new URL(url);
-        BufferedReader lector = new BufferedReader(new InputStreamReader(urlArchivo.openStream()));
-        String linea;
-        while ((linea = lector.readLine()) != null) {
-            System.out.println(linea);
-        }
-        lector.close();
+    public List<Profesor> getProfesores() {
+        return profesores;
     }
 
-    public static void main(String[] args) throws IOException {
-        String[] archivosCSV = new String[] {
-            "https://raw.githubusercontent.com/thxGalda/Proyecto-Gestion-de-Recursos/main/src/Profesores.csv",
-            "https://raw.githubusercontent.com/thxGalda/Proyecto-Gestion-de-Recursos/main/src/Carpetas.csv",
-            "https://raw.githubusercontent.com/thxGalda/Proyecto-Gestion-de-Recursos/main/src/Cursos.csv",
-            "https://raw.githubusercontent.com/thxGalda/Proyecto-Gestion-de-Recursos/main/src/Estudiantes.csv",
-            "https://raw.githubusercontent.com/thxGalda/Proyecto-Gestion-de-Recursos/main/src/Recursos.csv"
-        };
-        for (String archivoCSV :  archivosCSV) {
-            leerArchivoCSV(archivoCSV);
-        }
-        cargarArchivosCSV(archivosCSV);
+    public List<Curso> getCursos() {
+        return cursos;
     }
+
 }
